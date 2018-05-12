35b3b3905bdcb56e8ebcd1478bf932ccd02147b8
Joe <joe.ekiert@gmail.com>
2018-05-11 18:42:38 -0700

    just can not get the svg to trigger a new animation

--- reveal.js
+++ reveal.js
@@ -1,5 +1,27 @@
-console.log('JOE:');
+const array = [
+  'Strong',
+  'Loving',
+  'Caring',
+  'Kind'
+];
+
+function randomWord() {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 $('.hearts').on('click', function () {
-  console.log('howdy!');
+  $('tspan').remove();
+
+  const word = randomWord().split('');
+
+  word.forEach(l => {
+    console.log('JOE: l: ', l);
+    $('#text-container').append('<tspan>' + l + '</tspan>');
+  });
+
+  $('#svg').removeClass('happy').addClass('happy');
+  // $('.happy').beginElement();
+  // document.getElementById('svg').beginElement();
+  // $('#svg').detach().prependTo('body');
+
 });
